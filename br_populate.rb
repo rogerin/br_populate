68462b72c20c9d641920d6769ff00ccb62f77bf3
Patrick Espake <patrickespake@gmail.com>
2013-01-15 20:53:58 -0200

    alterado para funcionar somente com ruby 1.9, rails3 e para buscar o arquivo state.json do diretório db da aplicação em rails

--- br_populate.rb
+++ br_populate.rb
@@ -1,11 +1,8 @@
-require 'net/http'
-require 'net/https' # for ruby 1.8.7
 require 'json'
 
 module BRPopulate
   def self.states
-    http = Net::HTTP.new('raw.github.com', 443); http.use_ssl = true
-    JSON.parse http.get('/celsodantas/br_populate/master/states.json').body
+    ActiveSupport::JSON.decode(Rails.root.join("db", "states.json"))
   end
 
   def self.capital?(city, state)
@@ -14,10 +11,10 @@ module BRPopulate
 
   def self.populate
     states.each do |state|
-      state_obj = State.create(:acronym => state["acronym"], :name => state["name"])
+      state_obj = State.create(acronym: state["acronym"], name: state["name"])
 
       state["cities"].each do |city|
-        City.create(:name => city, :state => state_obj, :capital => capital?(city, state))
+        City.create(name: city, state: state_obj, capital: capital?(city, state))
       end
     end
   end
